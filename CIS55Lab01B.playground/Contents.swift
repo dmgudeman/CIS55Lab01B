285e3c9b18e4e84540f1c924e515b628e57905f3
David Gudeman <davidmgudeman@gmail.com>
2015-10-08 19:01:14 -0700

    corrected output added days

--- CIS55Lab01B.playground/Contents.swift
+++ CIS55Lab01B.playground/Contents.swift
@@ -12,12 +12,14 @@ calcDailyPay = dailyPay[index - 1] * 2
 dailyPay.append(calcDailyPay)
 }
 
+var day: Int = 0
 for value in dailyPay{
-  var Svalue: String = String(value)
+  ++day
+  var Svalue: String = "Day " + String(day) + ":   " + String(value)
   print(Svalue)
 }
 for index = 1; index < 31; ++index {
-  calcDailyTotal = Double(dailyPay[index] + dailyPay[index - 1])
+  calcDailyTotal = Double(dailyPay[index]) + dailyTotal[index - 1]
   dailyTotal.append(calcDailyTotal)
 }
 for value2 in dailyTotal{
